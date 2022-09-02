12ad62969f3939f760439c26b6a3456442617aee
ArimJung <lisa23@kakao.com>
2022-09-02 13:44:01 +0900

    컨트롤러 커뮤니티 보드액션 수정

--- src/main/java/ctrl/CommunityBoardAction.java
+++ src/main/java/ctrl/CommunityBoardAction.java
@@ -2,10 +2,14 @@ package ctrl;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import dao.BoardDAO;
+import dao.LlikeDAO;
 import set.BoardSet;
 import vo.BoardVO;
+import vo.LlikeVO;
+import vo.MemberVO;
 
 public class CommunityBoardAction implements Action{
 
@@ -14,17 +18,28 @@ public class CommunityBoardAction implements Action{
 		ActionForward forward = null;
 		BoardDAO dao = new BoardDAO();
 		BoardVO vo = new BoardVO();
-		
+		LlikeVO lvo = new LlikeVO(); 
+		LlikeDAO lDAO = new LlikeDAO(); 
 		
 //		vo.setSearchContent(searchContent);
 //		vo.setSearchCondition(searchCondition);
 		
-		BoardSet data = dao.sql_selectOne_BoardOne(vo);
+		vo.setBid(Integer.parseInt(request.getParameter("bid"))); // 게시글 번호
+		BoardSet data = dao.sql_selectOne_BoardOne(vo); // 게시글 상세 페이지 데이터
+		
+		HttpSession session=request.getSession();
+		MemberVO mvo = (MemberVO)session.getAttribute("member");
+		
+		lvo.setMid(mvo.getMid()); // 현재 접속한 멤버 id
+		lvo.setBid(Integer.parseInt(request.getParameter("bid")));
+		
+		lvo = lDAO.selectOne(lvo);
 		
 		if(data != null) {
 			request.setAttribute("data", data);
+			request.setAttribute("lvo", lvo);
 			forward=new ActionForward();
-			forward.setPath("/community.jsp");
+			forward.setPath("/communityBoard.jsp");
 			forward.setRedirect(false);
 		}
 		else {

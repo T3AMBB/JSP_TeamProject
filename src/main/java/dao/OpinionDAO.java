458c27c71bca70138d1e15a7775f19f3ebbc0b57
ArimJung <lisa23@kakao.com>
2022-09-03 19:22:04 +0900

    83번 set누락 수정

--- src/main/java/dao/OpinionDAO.java
+++ src/main/java/dao/OpinionDAO.java
@@ -1,30 +1,67 @@
 package dao;
 
-import java.sql.Connection; 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 import util.JDBCUtil;
-import vo.NovelVO;
 import vo.OpinionVO;
 
 public class OpinionDAO {
 	Connection conn;
 	PreparedStatement pstmt; 
 	final String sql_selectOne_O="SELECT * FROM OPINION LEFT OUTER JOIN MEMBER ON OPINION.MID=MEMBER.MID WHERE MEMBER.MID=?";
-	final String sql_selectAll_O="SELECT * FROM (SELECT A.*, ROWNUM AS RNUM FROM(SELECT * FROM OPINION B JOIN MEMBER M ON O.MID=M.MID ORDER BY OID DESC)A WHERE ROWNUM < = ?+10)WHERE RNUM  > = ?";
+	final String sql_selectAll_O="SELECT * FROM (SELECT A.*, ROWNUM AS RNUM FROM(SELECT * FROM OPINION B JOIN MEMBER M ON O.MID=M.MID ORDER BY O.OID DESC)A WHERE ROWNUM < = ?+10)WHERE RNUM  > = ?";
 
 	final String sql_selectAll_MYPAGE="SELECT * FROM (SELECT A.*, ROWNUM AS RNUM FROM(SELECT * FROM OPINION O JOIN NOVEL N ON O.NID=N.NID WHERE O.MID=? ORDER BY O.OID ASC) A WHERE ROWNUM < = ?+10)WHERE RNUM  > = ?";
 
-	final String sql_selectAll_OPINION_SEARCH="SELECT * FROM OPINION LEFT OUTER JOIN MEMBER ON OPINION.MID=MEMBER.MID ORDER BY OID DESC WHERE MEMBER.MID=?";
+	final String sql_selectAll_OPINION_SEARCH="SELECT * FROM OPINION LEFT OUTER JOIN MEMBER ON OPINION.MID=MEMBER.MID WHERE MEMBER.MID=? ORDER BY OID DESC";
 
-	final String sql_insert_O="INSERT INTO OPINION VALUES((SELECT NVL(MAX(OID),2000)+1 FROM OPINION),?,?,TO_DATE(sysdate,'yyyy-mm-dd hh24:mi'),?,?)";
+	final String sql_insert_O="INSERT INTO OPINION VALUES((SELECT NVL(MAX(OID),2000)+1 FROM OPINION),?,?,TO_CHAR(SYSDATE,'YYYY-MM-DD HH24:MI'),?,?)";
 	// INSERT INTO OPINION VALUES((서브쿼리),?,?,?)
 	final String sql_update_O="UPDATE OPINION SET CONTENT=? WHERE OID=?";
 	final String sql_delete_O="DELETE FROM OPINION WHERE OID=?";
+	final String sql_selectAll_novelBoard="SELECT * FROM OPINION LEFT OUTER JOIN MEMBER ON OPINION.MID=MEMBER.MID WHERE OPINION.NID=?";
 
+	
+	
+	public ArrayList<OpinionVO> selectAll_novelBoard(OpinionVO ovo){
+	      ArrayList<OpinionVO> datas=new ArrayList<OpinionVO>();
+	      conn=JDBCUtil.connect();
+	      System.out.println("로그: selectAll_novelBoard");
+//	      System.out.println(ovo.getNid());
+	      try {
+	         pstmt=conn.prepareStatement(sql_selectAll_novelBoard);
+	         pstmt.setInt(1, ovo.getNid());
+	         ResultSet rs=pstmt.executeQuery();
+//	         System.out.println("중간");
+	         while(rs.next()) {
+	            OpinionVO data=new OpinionVO();
+	            data.setOid(rs.getInt("OID"));
+	            data.setOcontent(rs.getString("OCONTENT"));
+	            data.setOdate(rs.getString("ODATE"));
+	            data.setOstar(rs.getInt("OSTAR"));
+	            data.setNid(rs.getInt("NID"));
+	            if(rs.getString("NICKNAME")==null) {
+	               data.setMid("[이름없음]");
+	            } else {
+	               // WRITER대신 MNAME을 담아서 WRITER를 뽑으면 MNAME이 출력된다.
+	               data.setMid(rs.getString("NICKNAME"));
+	            }
+	            datas.add(data);
+	         }
+	      } catch (SQLException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      } finally {
+	         JDBCUtil.disconnect(pstmt, conn);
+	      }
+//	      System.out.println("끝");
+
+	      return datas;
+	   }
 
 	public ArrayList<OpinionVO> selectAll_MYPAGE(OpinionVO ovo){ // 마이페이지에서 내가 작성한 리뷰 확인하기
 		ArrayList<OpinionVO> datas=new ArrayList<OpinionVO>();
@@ -43,6 +80,8 @@ public class OpinionDAO {
 				data.setOdate(rs.getString("ODATE"));
 				data.setMid(rs.getString("MID"));
 				data.setTitle(rs.getString("NTITLE"));
+				data.setNid(rs.getInt("NID"));
+				data.setOstar(rs.getInt("OSTAR"));
 				datas.add(data);
 			}
 		} catch (SQLException e) {
